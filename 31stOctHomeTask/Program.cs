29c39d66b73e568967bd152165112322e6092820
adwISAry <mi75axs6d@code.edu.az>
2023-10-31 23:00:27 +0400

    Add project files.

---
+++ 31stOctHomeTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using _31stOctHomeTask;
+
+int id = 0, age = 0;
+string fullname, groupNo;
+
+do
+{
+    Console.Write("ID daxil edin: ");
+    string idInput = Console.ReadLine();
+
+    Console.Write("Ad və soyad daxil edin (format: Ad Soyad): ");
+    fullname = Console.ReadLine();
+
+    Console.Write("Qrup nömrəsini daxil edin (format: PXXX): ");
+    groupNo = Console.ReadLine();
+
+}
+while (!Student.CheckGroupNo(groupNo) || !Student.CheckFullname(fullname) || id == 0 || age == 0);
+
+Student student = new Student(id, fullname, groupNo, age);
+
+Console.WriteLine("Tələblərə uyğun tələbə yaradıldı.");
+Console.WriteLine($"ID: {student.Id}");
+Console.WriteLine($"Ad və Soyad: {student.FullName}");
+Console.WriteLine($"Qrup nömrəsi: {student.GroupNo}");
+Console.WriteLine($"Yaş: {student.Age}");
---
+++ 31stOctHomeTask/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31stOctHomeTask
+{
+    public class Student
+    {
+
+        
+        public int Id {get ; set; }
+        public string FullName { get; set; }
+
+        public string GroupNo { get; set; }
+
+        public int Age { get; set; }
+
+        private static int nextId = 1;
+
+        public Student(int id, string fullname, string groupNo, int age)
+        {
+            Id = nextId++;
+            FullName = fullname;
+            GroupNo = groupNo;
+            Age = age;
+        }
+
+
+        public static bool CheckGroupNo(string groupNo)
+        {
+            if (groupNo.Length != 4 || !Char.IsLetter(groupNo[0]))
+
+                return false;
+            
+
+
+
+        }
+
+
+
+        public static bool CheckFullname(string fullname)
+        {
+            bool foundSpace = false;
+
+            foreach (char space in fullname)
+            {
+                if (space == ' ')
+                {
+                    foundSpace = true;
+                    break;
+                }
+            }
+
+            return foundSpace && !string.IsNullOrWhiteSpace(fullname);
+        }
+    }
+}
